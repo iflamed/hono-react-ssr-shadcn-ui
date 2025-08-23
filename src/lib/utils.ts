f9e3efd8b57bfd124d2dbb8c2bcb6ead80561567
iflamed <iflamed@gmail.com>
2025-08-23 23:05:04 +0800

    clean locale code

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,19 +1,6 @@
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
-import { languages } from '../locales'
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
-}
-
-export function getPath(req: Request) {
-  const url = new URL(req.url)
-  let pathname = url.pathname
-  for (let idx = 0; idx < languages.length; idx++) {
-    if (pathname.startsWith('/' + languages[idx] + '/')) {
-      pathname = pathname.replaceAll('/' + languages[idx] + '/', '/')
-      break
-    }
-  }
-  return pathname
 }
--- src/locales/index.ts
+++ src/locales/index.ts
@@ -17,4 +17,16 @@ i18n
 
 export const languages = Object.keys(files)
 
+export function getPath(req: Request) {
+  const url = new URL(req.url)
+  let pathname = url.pathname
+  for (let idx = 0; idx < languages.length; idx++) {
+    if (pathname.startsWith('/' + languages[idx] + '/')) {
+      pathname = pathname.replaceAll('/' + languages[idx] + '/', '/')
+      break
+    }
+  }
+  return pathname
+}
+
 export default i18n
